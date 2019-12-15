50ddfb039ac82ad00218f08ec26317315e65c7d4
Enmanuel Marval <emarval.fc@gmail.com>
2019-12-15 19:55:32 -0400

    agregado vista principal que redirige a las demas vistas

--- resources/views/tables/index.blade.php
+++ resources/views/tables/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Tu pedido logo</h2>
+                <a class="btn btn-info" href="{{ url('/') }}">Back</a>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('tables.create') }}"> Create New Table</a>
