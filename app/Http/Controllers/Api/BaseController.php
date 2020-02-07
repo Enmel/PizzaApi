97308c38c9188adad37e9b31fd507305c8fb1727
Enmanuel Marval <emarval.fc@gmail.com>
2020-02-07 17:15:16 -0400

    errores en spanish

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -27,7 +27,7 @@ class BaseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function sendError($error, $errorMessages = [], $code = 404)
+    public function sendError($error, $errorMessages = [], $code = 400)
     {
         $response = [
             'message' => $error,
