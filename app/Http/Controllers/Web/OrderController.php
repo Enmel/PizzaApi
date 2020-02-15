857e8cbe3a2f07d424d0f0bf1c0eee48d84d0264
Enmanuel Marval <emarval.fc@gmail.com>
2020-02-15 12:25:47 -0400

    cambiados recursos de comidas y controladores. Falta actualizar las vistas

--- app/Http/Controllers/Web/OrderController.php
+++ app/Http/Controllers/Web/OrderController.php
@@ -1,14 +1,11 @@
-<?php  
+<?php
 
 namespace App\Http\Controllers\WEB;
 
 use App\Http\Controllers\Controller;
 use App\Order;
-use App\OrderDetail;
-use Validator;
-use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
-  
+
 class OrderController extends Controller
 {
     public function index()
@@ -18,18 +15,18 @@ class OrderController extends Controller
         return view('orders.index', compact('orders'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
-   
+
     public function show(Order $order)
     {
         return view('orders.show', compact('order'));
     }
-   
+
     public function paidout(Order $order)
     {
-    	$order->paidout = (integer)!$order->paidout;
+        $order->paidout = (integer) !$order->paidout;
         $order->save();
 
-        if($order->paidout === 1){
+        if ($order->paidout === 1) {
             return redirect()->route('orders.index')->with('success', 'Orden pagada!');
         }
 
@@ -38,20 +35,18 @@ class OrderController extends Controller
 
     public function status(Order $order)
     {
-        if($order->status === 'pending'){
-        	$order->status = 'accepted';
-        	$order->save();
-            return redirect()->route('orders.index')->with('success', 'Orden pagada!');
+        if ($order->status === 'pending') {
+            $order->status = 'accepted';
+            $order->save();
+            return redirect()->route('orders.index')->with('success', 'Orden aceptada. Esperando pago');
         }
-
-        return redirect()->route('orders.index')->with('danger', 'Pago de orden ya confirmado.');
     }
-  
+
     public function destroy(Order $order)
     {
         $order->delete();
-  
+
         return redirect()->route('orders.index')
-                        ->with('success','Order deleted successfully');
+            ->with('success', 'Order deleted successfully');
     }
 }
--- app/Http/Resources/Food.php
+++ app/Http/Resources/Food.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\FoodCategory;
 
 class Food extends JsonResource
 {
@@ -15,14 +14,45 @@ class Food extends JsonResource
      */
     public function toArray($request)
     {
+        $prices = [];
+        $labels = [];
+
+        if ($this->very_small_price > 0) {
+            $prices['very_small'] = $this->very_small_price;
+            $labels['very_small'] = $this->categorydata->very_small_label;
+        }
+
+        if ($this->small_price > 0) {
+            $prices['small'] = $this->small_price;
+            $labels['small'] = $this->categorydata->small_label;
+        }
+
+        if ($this->medium_price > 0) {
+            $prices['medium'] = $this->medium_price;
+            $labels['medium'] = $this->categorydata->medium_label;
+        }
+
+        if ($this->large_price > 0) {
+            $prices['large'] = $this->large_price;
+            $labels['large'] = $this->categorydata->large_label;
+        }
+
+        if ($this->very_large_price > 0) {
+            $prices['very_large'] = $this->very_large_price;
+            $labels['very_large'] = $this->categorydata->very_large_label;
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'image' => $this->getFirstMedia('images')->getFullUrl(),
             'description' => $this->description,
-            'price' => $this->price,
-            'size' => $this->size,
-            'category' => new FoodCategory($this->categorydata)
+            'prices' => $prices,
+            'labels' => $labels,
+            'category' => [
+                'id' => $this->categorydata->id,
+                'name' => $this->categorydata->name,
+            ],
         ];
     }
 }
