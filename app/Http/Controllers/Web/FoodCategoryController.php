c4d1b0571c63112173505ae499405c88761ec83d
Enmanuel Marval <emarval.fc@gmail.com>
2020-02-15 09:56:37 -0400

    cambios en el controlador web de las categorias para soportar labels y hacer imagenes obligatorias

--- app/Http/Controllers/Web/FoodCategoryController.php
+++ app/Http/Controllers/Web/FoodCategoryController.php
@@ -1,73 +1,84 @@
-<?php  
+<?php
 
 namespace App\Http\Controllers\WEB;
 
-use App\Http\Controllers\Controller;
 use App\FoodCategory;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-  
+
 class FoodCategoryController extends Controller
 {
     public function index()
     {
         $foodcategories = FoodCategory::paginate(15);
-  
-        return view('foodcategories.index',compact('foodcategories'))
+
+        return view('foodcategories.index', compact('foodcategories'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
-   
+
     public function create()
     {
         return view('foodcategories.create');
     }
-  
+
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|max:256|string'
+            'name' => 'required|max:256|string',
+            'very_small_label' => 'required|max:256|string',
+            'small_label' => 'required|max:256|string',
+            'medium_label' => 'required|max:256|string',
+            'large_label' => 'required|max:256|string',
+            'very_large_label' => 'required|max:256|string',
         ]);
-  
-        $category = FoodCategory::create($request->all());
 
-        if($request->hasFile('image')){
-            $category->addMediaFromRequest('image')->toMediaCollection('images');
+        if (!$request->hasFile('image')) {
+            return redirect()->route('foodcategories.index')->with('error', 'Debe cargar una imagen');
         }
-   		
+
+        $category = FoodCategory::create($request->all());
+        $category->addMediaFromRequest('image')->toMediaCollection('images');
+
         return redirect()->route('foodcategories.index')->with('success', 'FoodCategory Created Successfully!');
     }
-   
+
     public function show(FoodCategory $foodcategory)
     {
-        return view('foodcategories.show',compact('foodcategory'));
+        return view('foodcategories.show', compact('foodcategory'));
     }
-   
+
     public function edit(FoodCategory $foodcategory)
     {
-        return view('foodcategories.edit',compact('foodcategory'));
+        return view('foodcategories.edit', compact('foodcategory'));
     }
-  
+
     public function update(Request $request, FoodCategory $foodcategory)
     {
         $request->validate([
-            'name' => 'required|max:256|string'
+            'name' => 'required|max:256|string',
+            'very_small_label' => 'required|max:256|string',
+            'small_label' => 'required|max:256|string',
+            'medium_label' => 'required|max:256|string',
+            'large_label' => 'required|max:256|string',
+            'very_large_label' => 'required|max:256|string',
         ]);
 
-        if($request->hasFile('image')){
+        if ($request->hasFile('image')) {
             $foodcategory->clearMediaCollection('images');
             $foodcategory->addMediaFromRequest('image')->toMediaCollection('images');
         }
 
         $foodcategory->update($request->all());
-  
+
         return redirect()->route('foodcategories.index')
-                        ->with('success','FoodCategory updated successfully');
+            ->with('success', 'FoodCategory updated successfully');
     }
-  
+
     public function destroy(FoodCategory $foodcategory)
     {
         $foodcategory->delete();
-  
+
         return redirect()->route('foodcategories.index')
-                        ->with('success','FoodCategory deleted successfully');
+            ->with('success', 'FoodCategory deleted successfully');
     }
 }
