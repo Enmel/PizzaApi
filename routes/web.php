01a1326ef347a06a53839db8452e081b8b6f0440
Enmanuel Marval <emarval.fc@gmail.com>
2020-02-17 11:21:24 -0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::resource('foods','Web\FoodController');
 Route::get('/orders/{order}/paidout', 'Web\OrderController@paidout')->name('orders.paidout');
 Route::get('/orders/{order}/status', 'Web\OrderController@status')->name('orders.status');
 Route::get('/orders/{order}/vouchers', 'Web\OrderController@vouchers')->name('orders.vouchers');
-Route::get('/orders/{order}/vouchers/{orderVoucher}/', 'Web\OrderController@confirmPaid')->name('orders.confirmPaid');
+Route::get('/orders/{order}/vouchers/{orderVoucher}', 'Web\OrderController@confirmPaid')->name('orders.confirmPaid');
 Route::resource('orders','Web\OrderController');
 Route::get('/reservations/{reservation}/status', 'Web\ReservationController@status')->name('reservations.status');
 Route::resource('reservations','Web\ReservationController');
